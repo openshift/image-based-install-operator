8ee0e534040fd81780375ab9f18e9026a24a4604
Igal Tsoiref <itsoiref@redhat.com>
2024-09-14 21:13:03 +0300

    MGMT-18894: Remove support for ICI without BMH

--- hack/integration-test/validate.sh
+++ hack/integration-test/validate.sh
@@ -4,8 +4,8 @@ set -ex
 
 echo "Waiting for configuration ISO URL to be set on ImageClusterInstall ibi-test/ibi-test"
 
-for i in {1..60}; do
-  url=$(oc get -n ibi-test imageclusterinstall ibi-test -ojsonpath='{.status.configurationImageURL}')
+for i in {1..120}; do
+  url=$(oc get -n ibi-test dataimage ostest-extraworker -ojsonpath='{.spec.url}' || true)
   if [[ -n "$url" ]]; then
     break
   fi
