2d756c134461e73e2c78948440af686f1a4d1a77
Elior Erez <eerez@redhat.com>
2024-09-19 18:02:07 +0300

    MGMT-18909: Delete DataImage on ICI deletion

--- hack/integration-test/validate.sh
+++ hack/integration-test/validate.sh
@@ -16,3 +16,24 @@ if [[ -z "$url" ]]; then
   echo "ERROR: configurationImageURL on ImageClusterInstall ibi-test/ibi-test was not set within 60 seconds"
   exit 1
 fi
+
+echo "Waiting for deleting data image after deletion of ici"
+oc get -n ibi-test dataimage ostest-extraworker
+oc delete imageclusterinstalls.extensions.hive.openshift.io -n ibi-test ibi-test --wait=false
+
+for i in {1..30}; do
+  res=$(oc get -n ibi-test dataimage ostest-extraworker 2>&1 || true)
+  echo $res | grep NotFound
+  ret=$?
+  if [[ $ret -eq 0 ]]; then
+    echo "DataImage was removed successfully"
+    break
+  fi
+  echo "Found existing DataImage, waiting for delete ($i)..."
+  sleep 1
+done
+
+if [[ $ret -ne 0 ]]; then
+  echo "ERROR - Failed tp remove DataImage on ICI deletion"
+  exit 1
+fi
