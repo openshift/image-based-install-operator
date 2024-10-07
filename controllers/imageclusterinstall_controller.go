38a2cdeb633c7bd87a572c76a190c25c45ee45d3
Eran Cohen <eranco@redhat.com>
2024-10-07 21:19:49 +0300

    MGMT-18973 IBIO serviceaccount does not have proper permissions to patch extraManifests configmap resources Allow IBIO to patch configMaps in all namespaces
    Signed-off-by: Eran Cohen <eranco@redhat.com>

--- controllers/imageclusterinstall_controller.go
+++ controllers/imageclusterinstall_controller.go
@@ -115,7 +115,7 @@ const (
 )
 
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;update;patch
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;update;patch,namespace=*
 //+kubebuilder:rbac:groups=extensions.hive.openshift.io,resources=imageclusterinstalls,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=extensions.hive.openshift.io,resources=imageclusterinstalls/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=extensions.hive.openshift.io,resources=imageclusterinstalls/finalizers,verbs=update
