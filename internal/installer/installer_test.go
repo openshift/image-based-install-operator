633d6709dfed873a711defc4a18741315568b52f
Nick Carboni <ncarboni@redhat.com>
2024-11-27 16:43:18 -0500

    Create an installer suite test and remove existing failing test
    The comment in the `proxy` implementation suggests that no validation
    should happen here as the logic for interpreting the proxy values
    correctly is handled in LCA

--- internal/installer/installer_test.go
+++ internal/installer/installer_test.go
@@ -8,23 +8,11 @@ import (
 )
 
 var _ = Describe("proxy", func() {
-
-	BeforeEach(func() {
-	})
-
 	It("Proxy is nil, nothing to change", func() {
 		Expect(proxy(nil)).To(BeNil())
 	})
 
 	It("If https and http proxy were not set, nothing to set", func() {
 		Expect(proxy(&v1alpha1.Proxy{})).To(BeNil())
-	})
-
-	It("Verify no proxy was set", func() {
-		iciProxy := &v1alpha1.Proxy{HTTPSProxy: "aaa", NoProxy: "test"}
-		proxy := proxy(iciProxy)
-		Expect(proxy.HTTPSProxy).To(Equal(iciProxy.HTTPSProxy))
-		Expect(proxy.NoProxy).To(Equal(iciProxy.NoProxy))
-		Expect(proxy.NoProxy).ToNot(Equal(iciProxy.NoProxy))
 	})
 })
